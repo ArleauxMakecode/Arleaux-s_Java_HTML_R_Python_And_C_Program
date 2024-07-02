592d5c7d37e33ca15309cd9480a995baf866507f
ArleauxMakecode <120610871+ArleauxMakecode@users.noreply.github.com>
2024-07-02 19:20:38 -0400

    rji

--- build.py
+++ build.py
@@ -4,4 +4,5 @@ print(2024, ArI, Inc, sep = ' ')
 var1 = input("What is your name:")
 hello = 'Hello'
 print(hello, var1, sep =' ')
-
+var2 = input("What is your favorite food")
+print(I, heard, you, like, var2, sep =' ')
