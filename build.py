afbfa2306e1db76c8f1b2055846b1066e3259f66
ArleauxMakecode <120610871+ArleauxMakecode@users.noreply.github.com>
2024-07-02 12:05:54 -0400

    done

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
-print('2024,ArI,Inc.')
+ArI = 'ArI'
+Inc = 'Inc.'
+print(2024, ArI, Inc, sep = ' ')
 var1 = input("What is your name:")
-var2 = input("What is your favorite food:")
-print("Hello")
-print(var1)
-print(/nI heard you like)
-print(var2)
+hello = 'Hello'
+print(hello, var1, sep =' ')
+
