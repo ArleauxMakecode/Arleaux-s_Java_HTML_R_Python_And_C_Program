7859629d8ca575dab03c7a5563ff0f3d7de82fe6
ArleauxMakecode <120610871+ArleauxMakecode@users.noreply.github.com>
2024-07-02 19:15:10 -0400

    done

--- build.r
+++ build.r
@@ -2,3 +2,9 @@ mard <- 32040 - 9424
 cdp <- mard - 849
 print(mard)
 print(cdp)
+nfcxvds <- 838274375 + cdp
+print(nfcxvds)
+sdid <- nfcxvds - 43945036
+df <- sdid - 325244254
+print(sdid)
+print(df)
